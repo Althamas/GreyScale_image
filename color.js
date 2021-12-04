8df284403069dd66d5af8c41b02662b6ca63e099
Althamas <althamaz292@gmail.com>
2021-12-04 13:11:25 +0530

    Initial commit

---
+++ color.js
@@ -0,0 +1,18 @@
+function changecolor(){
+    var dd1 = document.getElementById("div1")
+    var dd2 = document.getElementById("div2")
+    dd1.className = "greenback"
+    dd2.className = "yellowback"
+}
+
+function changetext(){
+    var ddt1 = document.getElementById("div1")
+    var ddt2 = document.getElementById("div2")
+    ddt2.innerHTML = "Althamas";
+    ddt1.innerHTML = "Nazma";
+}
+
+function back(){
+    dd1.innerHTML = "ffff"
+
+}
